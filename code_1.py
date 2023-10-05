f61878c81cc9f2de5f19b881dda79791f2769132
Mila <m9234434@gmail.com>
2023-10-05 23:16:36 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,38 +1,52 @@
-def generate_random_data():
-import random
-        print(f"Random Number: {item}")
+        }
+        for (int i = 0; i < 10; i++) {
+        }
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int item : data) {
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        int[] data = new int[10];
 
+        print(f"Random Number: {item}")
+    return data
 if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
+    for item in data:
+    main()
 
 
 def main():
-    main()
+def generate_random_data():
+
+
+    data = generate_random_data()
+import randomimport random
+    data = generate_random_data()
+
     return data
+def main():
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+if __name__ == "__main__":
+    main()
     for item in data:
 
-        int[] data = new int[10];
-        }
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
-        }
-public class RandomDataGenerator {
-    }
-    public static void main(String[] args) {
-}
-        for (int i = 0; i < 10; i++) {
+    data = [random.randint(1, 100) for _ in range(10)]def main():
+    data = generate_random_data()
 
-            data[i] = (int) (Math.random() * 100) + 1;        int[] data = new int[10];
 
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-    }
-    public static void main(String[] args) {
-}
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
+    for item in data:
+        print(f"Random Number: {item}")
 
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
-        }
+    return data
+import random
+def generate_random_data():
+if __name__ == "__main__":
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
