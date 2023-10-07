5ff6454028a8d0f7d57a9a8588a53ea4ab20a9ba
Mila <m9234434@gmail.com>
2023-10-07 05:28:37 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,64 @@
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+public class RandomDataGenerator {
+}
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        }
+    }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {    return data
+
+import random
+    for item in data:
+def main():
+
+        print(f"Random Number: {item}")
+    main()
+    data = generate_random_data()
+if __name__ == "__main__":
+
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]        for (int item : data) {
+}
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+
+
+    }
+        for (int i = 0; i < 10; i++) {
+        }
+        int[] data = new int[10];    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+import random
+        print(f"Random Number: {item}")
+    main()
+    data = generate_random_data()
+
+def generate_random_data():
+def main():
+    for item in data:
+
+
+    return data
+    main()
+
+def main():
+    for item in data:
+import random
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    data = generate_random_data()
+
+def generate_random_data():
+    return data
