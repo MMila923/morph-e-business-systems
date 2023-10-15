11ee20a8aaff31bf2a884c5fb4dd74ceca600fc5
Mila <m9234434@gmail.com>
2023-10-16 01:20:43 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,49 +1,26 @@
+def main():
+if __name__ == "__main__":
+def generate_random_data():
+    data = generate_random_data()
+    for item in data:
+
+
+
+        print(f"Random Number: {item}")
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    return data        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        }
+
+        }
+public class RandomDataGenerator {
+    }
+
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
             data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
 }
-        for (int item : data) {
-    }
-        }
-        int[] data = new int[10];
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-
-        }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
-    }
-            System.out.println("Random Number: " + item);
-}
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {
-        int[] data = new int[10];
-
-}
-
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-        int[] data = new int[10];
-        }
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
-
-    public static void main(String[] args) {
-    }
-        }
-            data[i] = (int) (Math.random() * 100) + 1;            data[i] = (int) (Math.random() * 100) + 1;
-        }
-
-            System.out.println("Random Number: " + item);
-
-}
-    }
-    public static void main(String[] args) {
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-        }
-public class RandomDataGenerator {
-        for (int item : data) {
