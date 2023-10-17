83ec54197e495419e1198fa4f54493ded9bde725
Mila <m9234434@gmail.com>
2023-10-17 07:38:19 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,29 @@
-    main()
-def main():
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+}
+    }
+
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+        for (int item : data) {
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    return data;
+    });
+    const data = [];
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    data.forEach(item => {
+function main() {
+}
 
 
-import random
 
-
-    return data
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-if __name__ == "__main__":
-    data = generate_random_data()
-        print(f"Random Number: {item}")
+    for (let i = 0; i < 10; i++) {
+main();
+    const data = generateRandomData();
